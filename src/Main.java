96d54029a40653013b0958dffab5113b75d47ab3
Anastasia.L <easy.club.info@gmail.com>
2024-04-13 21:19:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weight = 98;
+        System.out.println("Ваш вес: " + weight + "кг");
+        double height = 1.87;
+        System.out.println("Ваш рост: " + height + "метра");
+        int index = service.calculate(weight, height);
+        System.out.println("Индекс Вашего тела: " + index);
+    }
+}
+
+}
